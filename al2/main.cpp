56c39889396acb2de5746533d59e03a8d933d518
USSRER <847484392@qq.com>
2018-08-20 22:37:42 +0800

    22_37

--- al2/main.cpp
+++ al2/main.cpp
@@ -1,24 +1,48 @@
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-#include <iostream>
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int count1=0;
-        int count2=0;
-    while(true){
-        if((l1->next==NULL)&(l2->next==NULL)){
-            break;
-        }
-        count2++;
-    }
-        int temp=0;
-        for (int i = 0; i < count2; ++i) {
-
-        }
-    }
-};
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+  class Solution {
+  public:
+      ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *p,*q,*res,*head;
+          p=l1;
+          q=l2;
+          res=new ListNode(0);
+          head=new ListNode(0);
+          head=res;
+          int temp=0;
+          while((p->next!= nullptr)&&(q->next!= nullptr)){
+              temp=p->val+q->val+temp;
+              ListNode *te=new ListNode(temp%10);
+              temp=temp/10;
+              res->next=te;
+              res=te;
+              p=p->next;
+              q=q->next;
+          }
+          while(p->next!= nullptr){
+              temp+=p->val;
+              ListNode *te=new ListNode(temp%10);
+              temp=temp/10;
+              res->next=te;
+              res=te;
+              p=p->next;
+          }
+          while(q->next!= nullptr){
+              temp+=q->val;
+              ListNode *te=new ListNode(temp%10);
+              temp=temp/10;
+              res->next=te;
+              res=te;
+              q=q->next;
+          }
+          if(temp==1){
+              ListNode *te=new ListNode(1);
+              res->next=te;
+              res=te;
+          }
+          return head->next;
+      }
+  };
---
+++ al3/cmake-build-debug/CMakeFiles/al3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/al3.dir/main.cpp.o"
+  "al3.pdb"
+  "al3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/al3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
